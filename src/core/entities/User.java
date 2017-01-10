e93fea9a9e57318d879a769d4f9da1d04a6b3659
Wylliam <wylliam.delaunay@gmail.com>
2017-01-10 15:58:39 +0100

    Add User class

---
+++ src/core/entities/User.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class User {
+	private String name;
+	private String lastname;
+	private String login;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
