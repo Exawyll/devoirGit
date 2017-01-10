9cb25a3864969a6452027e08141e17e0197dadc6
DANIEL-Dereck <daniel.dereck@gmail.com>
2017-01-10 16:38:36 +0100

    add attributs / getteur / setter into Droits

--- src/core/entities/Droits.java
+++ src/core/entities/Droits.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Droits {
-
+	private String niveau;
+	private String severite;
+	private String nom;
+	private String societe;
+	
+	public String getNiveau() {
+		return niveau;
+	}
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+	public String getSeverite() {
+		return severite;
+	}
+	public void setSeverite(String severite) {
+		this.severite = severite;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getSociete() {
+		return societe;
+	}
+	public void setSociete(String societe) {
+		this.societe = societe;
+	}
 }
