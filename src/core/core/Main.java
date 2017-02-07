d150588a3701d4e6e96f335df08f33aef7d7ea4b
Wylliam <wylliam.delaunay@gmail.com>
2017-02-07 15:20:47 +0100

    add conf

--- src/core/core/Main.java
+++ src/core/core/Main.java
@@ -1,8 +1,12 @@
 package core;
 
+import json.JsonManager;
+import utils.Configuration;
+
 public class Main {
 		
 	public static void main(String[] args) {
+		JsonManager.getInstance().readFromFile("conf.prog", "/", Configuration.class);
 		
 	}
 }
